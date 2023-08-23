56af199b8c4f7d6aef73a91458b09fde23ef6560
Sparsegull2 <142937470+Sparsegull2@users.noreply.github.com>
2023-08-22 21:23:49 -0400

    Add files via upload

---
+++ Lab2C.cs
@@ -0,0 +1,26 @@
+﻿/* 
+Class: CSE 1321L 
+Section: #03 
+Term: Fall 2023
+Instructor: John Blake 
+Name: Christopher Morrison
+Lab#: ... 
+*/
+
+using System;
+
+class Lab2C{
+    public static void Main(string[] args){
+        Console.Write("Enter a width: ");
+        int width = int.Parse(Console.ReadLine());
+        Console.Write("Enter a height: ");
+        int height = int.Parse(Console.ReadLine());
+
+        int area = width * height;
+        int perimeter = 2* (width + height);
+
+        Console.WriteLine("The area is " + area);
+        Console.WriteLine("The perimeter is " + perimeter);
+
+    }
+}
