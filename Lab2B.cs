f3b6cadc1815f6c55409c52e303df73396be17bd
Sparsegull2 <142937470+Sparsegull2@users.noreply.github.com>
2023-08-22 21:15:17 -0400

    Add files via upload

---
+++ Lab2B.cs
@@ -0,0 +1,17 @@
+﻿/* 
+Class: CSE 1321L 
+Section: #03 
+Term: Fall 2023
+Instructor: John Blake 
+Name: Christopher Morrison
+Lab#: Lab2B
+*/
+
+using System;
+
+class Lab2B{
+    public static void Main(string[] args){
+
+        Console.WriteLine("___*___\n__*_*__\n_*_*_*_\n*_*_*_*\n_*_*_*_\n__*_*__\n___*___");
+    }
+}
